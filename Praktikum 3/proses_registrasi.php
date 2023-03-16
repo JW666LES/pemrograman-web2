5f4506a3376ea43fcfde93bbcece01fca87f52fa
JW666LES <114549241+JW666LES@users.noreply.github.com>
2023-03-16 14:07:25 +0700

    Add files via upload

---
+++ Praktikum 3/proses_registrasi.php
@@ -0,0 +1,20 @@
+<?php
+
+$domisili = ["jakarta", "bogor", "depok", "tanggerang", "Bekasi"];
+$program_studi = [
+    "SI"=>"Sistem Informasi",
+    "TI"=>"Teknik Informatika",
+    "BD"=>"Bisnis Digital",
+];
+
+$skills = [
+    "HTML"=>10,
+    "CSS"=>10,
+    "javascript"=>20,
+    "RWB Boootstrap"=>20,
+    "PHP"=>30,
+    "Python"=>30,
+    "Java"=>50,
+];
+
+?>
